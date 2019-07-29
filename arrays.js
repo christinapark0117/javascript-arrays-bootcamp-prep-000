f211747871640419c43db95bbddf4ea20c292b99
Christina Park <christinapark0117@gmail.com>
2019-07-29 06:04:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,5 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  array = array.slice(1)
+  return array
 }
